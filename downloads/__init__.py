9a765a87038c44a240de4fa8778e299907d99f7f
hedleyroos <hedleyroos@gmail.com>
2012-05-21 12:26:32 +0200

    Safely create dir

--- downloads/__init__.py
+++ downloads/__init__.py
@@ -8,8 +8,8 @@ from downloads.models import TEMP_ROOT
 
 # create temporary downloads folder if it does not exist
 tmp_abs_path = os.path.join(settings.MEDIA_ROOT, TEMP_ROOT)
-while not os.path.exists(tmp_abs_path):
+if not os.path.exists(tmp_abs_path):
     try:
-        os.makedir(tmp_abs_path)
-    except:
+        os.mkdir(tmp_abs_path)
+    except OsError:
         pass
