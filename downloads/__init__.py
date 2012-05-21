868429b19388b5fe7c2d57b64019b32570925d64
Rizmari Versfeld <rizmari@praekelt.com>
2012-05-21 11:55:57 +0200

    create tmp folder on startup

---
+++ downloads/__init__.py
@@ -0,0 +1,15 @@
+import os
+import os.path
+
+from django.conf import settings
+
+from downloads.models import TEMP_ROOT
+
+
+# create temporary downloads folder if it does not exist
+tmp_abs_path = os.path.join(settings.MEDIA_ROOT, TEMP_ROOT)
+while not os.path.exists(tmp_abs_path):
+    try:
+        os.makedir(tmp_abs_path)
+    except:
+        pass
