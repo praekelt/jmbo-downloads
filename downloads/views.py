7e45b25283f5567dc041fd9008560331c4717765
Rizmari Versfeld <rizmari@praekelt.com>
2012-05-25 02:20:53 +0200

    create a hierarchically sorted list of downloads

--- downloads/views.py
+++ downloads/views.py
@@ -35,14 +35,66 @@ def download_request(request, slug):
     return response
 
 
+def get_full_category(category, full_name):
+    if not category:
+        return full_name
+    else:
+        return get_full_category(category.parent, category.title + full_name)
+
+
 class ObjectList(GenericObjectList):
 
     def get_extra_context(self, *args, **kwargs):
-        return {'title': _('Downloads')}
-
+        dls = Download.permitted.all()
+        # re-order hierarchically
+        sort_list = []
+        index = 0
+        cat = None
+        full_cat = ''
+        for dl in dls:
+            if dl.primary_category != cat:
+                cat = dl.primary_category
+                full_cat = get_full_category(cat, '')
+            sort_list.append((full_cat, index))
+            index += 1
+        # perform insertion sort on full category name
+        for i in range(1, len(sort_list)):
+            val = sort_list[i]
+            j = i - 1
+            while j >= 0 and sort_list[j][0] > val[0]:
+                sort_list[j + 1] = sort_list[j]
+                j -= 1
+            sort_list[j + 1] = val
+        sorted_dls = []
+        for tup in sort_list:
+            sorted_dls.append(dls[tup[1]])
+        return {'title': _('Downloads'), 'sorted_objects':sorted_dls}
+    
     def get_queryset(self, *args, **kwargs):
-        
-        return Download.permitted.all()
+        dls = Download.permitted.all()
+        # re-order hierarchically
+        sort_list = []
+        index = 0
+        cat = None
+        full_cat = ''
+        for dl in dls:
+            if dl.primary_category != cat:
+                cat = dl.primary_category
+                full_cat = get_full_category(cat, '')
+            sort_list.append((full_cat, index))
+            index += 1
+        # perform insertion sort on full category name
+        for i in range(1, len(sort_list)):
+            val = sort_list[i]
+            j = i - 1
+            while j >= 0 and sort_list[j][0] > val[0]:
+                sort_list[j + 1] = sort_list[j]
+                j -= 1
+            sort_list[j + 1] = val
+        sorted_dls = []
+        for tup in sort_list:
+            sorted_dls.append(dls[tup[1]])
+        return sorted_dls
 
     def get_paginate_by(self, *args, **kwargs):
         return 20
