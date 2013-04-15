23fd821d32e7d1849cffe319653068c5dc5c79e7
Rizmari Versfeld <rizmari@praekeltconsulting.com>
2013-04-15 18:59:12 +0200

    add settings to specify whether to redirect on download, or serve locally

--- setup.py
+++ setup.py
@@ -17,10 +17,11 @@ setup(
     ],
     include_package_data=True,
     tests_require=[
+        'jmbo>=1.0',
         'django-setuptest',
         'coverage',
     ],
-    test_suite="setuptest.SetupTestSuite",
+    test_suite="setuptest.setuptest.SetupTestSuite",
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
