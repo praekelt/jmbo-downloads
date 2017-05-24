8b899f26f8b19cbb8ed7ddb275e0017db9ca6ab1
cblignaut <cilliers@praekeltconsulting.com>
2017-05-24 14:36:03 +0200

    Added coveralls

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jmbo-downloads",
-    version="2.0.2",
+    version="3.0.0",
     description="Downloads for Jmbo.",
     long_description=(open("README.rst", "r").read() +
                         open("AUTHORS.rst", "r").read() +
